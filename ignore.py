9412f253796fbf4861f57f00fd6d82f14ef13319
Aaron Cooper <dev@aaroncooper.ca>
2023-12-12 08:59:54 -0500

    .manualgitignore -> .mgitignore

--- ignore.py
+++ ignore.py
@@ -2,8 +2,8 @@ import os
 
 def get_ignore(root: str):
     ignore_paths = set(['.git'])
-    if os.path.isfile(root + '/.manualgitignore'):
-        with open(root + '/.manualgitignore', "r") as ignore_file:
+    if os.path.isfile(root + '/.mgitignore'):
+        with open(root + '/.mgitignore', "r") as ignore_file:
             for line in ignore_file.readlines():
                 line = line.strip()
                 ignore_paths.add(line)
